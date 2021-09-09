7d918f95c9153485277b67d49f847cc61b9a1e6d
Mads Smed <ms@humanrisks.com>
2021-09-09 10:21:59 +0200

    implement back queue in crawler

--- Crawler.py
+++ Crawler.py
@@ -3,19 +3,27 @@ from urllib.robotparser import RobotFileParser
 import requests
 from bs4 import BeautifulSoup
 
+from BackQueues import BackQueues
 from Frontier import Frontier
+from UrlParser import get_hostname
 
 
 class Crawler:
     def __init__(self, seeds):
         self.frontier = Frontier(seeds)
+        self.back_queues = BackQueues()
+        self.heap = []
 
     @staticmethod
-    def crawl_allowed(link):
-        rp = RobotFileParser()
-        rp.set_url(link)
-        rp.read()
-        return rp.can_fetch("*", link)
+    def crawl_allowed(url):
+        try:
+            print(url)
+            rp = RobotFileParser()
+            rp.set_url(url)
+            rp.read()
+            return rp.can_fetch("*", url)
+        except:
+            return False
 
     @staticmethod
     def extract_urls(doc: BeautifulSoup):
@@ -28,11 +36,17 @@ class Crawler:
                 valid_urls.append(a['href'])
         return valid_urls
 
+    @staticmethod
+    def query_html(url):
+        response = requests.get(url)
+        return BeautifulSoup(response.text, 'html.parser')
+
     def crawl(self):
         while len(self.frontier) > 0:
-            link = self.frontier.get_url()
-            if self.crawl_allowed(link):
-                r = requests.get(link)
-                doc = BeautifulSoup(r.text, 'html.parser')
-                self.frontier.add(self.extract_urls(doc))
-                print(doc.find('title').text)
+            url = self.frontier.get_url()
+            if self.crawl_allowed(url):
+                hostname = get_hostname(url)
+                parsed_html = self.query_html(url)
+                new_urls = self.extract_urls(parsed_html)
+
+                self.back_queues.add_urls(hostname, new_urls)
