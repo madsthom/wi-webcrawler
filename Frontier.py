33d3b86c0c7f73b9d4c48b739b4e07dc822586a9
Mads Smed <ms@humanrisks.com>
2021-09-09 10:21:16 +0200

    Refactor: functions for add single url and multiple

--- Frontier.py
+++ Frontier.py
@@ -11,8 +11,11 @@ class Frontier:
     def get_url(self):
         return self.front_queue.pop(0)
 
-    def add(self, url: list):
-        self.front_queue.extend(url)
+    def add(self, url):
+        self.front_queue.append(url)
+
+    def add_urls(self, urls: list):
+        self.front_queue.extend(urls)
 
     def __len__(self):
         return len(self.front_queue)
